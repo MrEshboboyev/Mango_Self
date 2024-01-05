a887ac63fb3a136414939bb0f6ae8da46e1523c1
mreshboboyev <mreshboboyev@gmail.com>
2024-01-05 21:04:08 +0500

    DI for AppDbContext in CouponAPIController.cs; Get() method writed CouponAPIController.cs (Function : Get All Coupons from Database); Get(int id) method writed CouponAPIController.cs (Function : Get By Id from Database of CouponTable)

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -7,5 +9,33 @@ namespace Mango.Services.CouponAPI.Controllers
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+           _db = db;
+        }
+
+
+        // get all entities
+        [HttpGet]
+        public IEnumerable<Coupon> Get()
+        {
+            IEnumerable<Coupon> objList = _db.Coupons.ToList();
+            return objList;
+        }
+
+        // get entity by id 
+        [HttpGet]
+        [Route("{id:int}")]
+        public Coupon Get(int id)
+        {
+            Coupon obj = _db.Coupons.FirstOrDefault(c => c.CouponId == id);
+            if(obj == null)
+            {
+                return new Coupon();
+            }
+            return obj;
+        }
     }
 }
