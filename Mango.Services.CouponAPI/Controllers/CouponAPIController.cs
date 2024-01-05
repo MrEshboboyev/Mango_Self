1ee703cc4ad6cd643811fec8b8354c5154cb7a1e
mreshboboyev <mreshboboyev@gmail.com>
2024-01-05 20:55:45 +0500

    Creating CouponAPIController.cs type of "APIController - Empty";

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/coupon")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+    }
+}
