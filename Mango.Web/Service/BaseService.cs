b73d45deb7b16fff861707da4d8a7c4df68bd0d3
mreshboboyev <mreshboboyev@gmail.com>
2024-01-06 00:33:00 +0500

    Creating Service/BaseService.cs  and BaseService.cs implement that IBaseService.cs and implement SendAysnc() with NotImplementedException()

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        public Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
