47bed21019981e4185f31d44dc17a4a7aef06590
mreshboboyev <mreshboboyev@gmail.com>
2024-01-05 20:13:39 +0500

    Creating Data/AppDbContext.cs and Default settings for AppDbContext.cs ; Creating DbSet<Coupon>

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        // creating Coupons table in Db 
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
