0d191e34219f6b9336f872e6a125e09a9d8d4c31
mreshboboyev <mreshboboyev@gmail.com>
2024-01-06 13:18:41 +0500

    Configuring HttpClient and HttpContextAccessor, add HttpClient<ICouponService, CouponService>() and Service Lifetimes added of AddScoped<>: IBaseService, ICouponService in Program.cs

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,9 +1,23 @@
+using Mango.Web.Service;
+using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// add HttpClient
+builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+
+// for couponService
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+
+
+// service lifetime for IBaseService, ICouponService
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 // CouponAPIBase populated
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
