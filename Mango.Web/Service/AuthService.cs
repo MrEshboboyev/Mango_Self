75d326fd0a3a6c22e48acded8880b88a005f1be5
mreshboboyev <mreshboboyev@gmail.com>
2024-01-06 22:38:16 +0500

    Create Service/AuthService.cs and inherit from IAuthService.cs; DI for IBaseService;

---
+++ Mango.Web/Service/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
