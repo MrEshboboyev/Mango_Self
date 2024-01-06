f8d909c920403ea2e0d7b60e7467635a3dc6a1a1
mreshboboyev <mreshboboyev@gmail.com>
2024-01-06 19:59:52 +0500

    'claimList' variable created from three claims list : (Name, Email, Id) in GenerateToken() method of JwtTokenGenerator

--- Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
+++ Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -2,6 +2,7 @@
 using Mango.Services.AuthAPI.Models.Dto;
 using Mango.Services.AuthAPI.Service.IService;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Mango.Services.AuthAPI.Service
@@ -20,6 +21,13 @@ namespace Mango.Services.AuthAPI.Service
             var tokenHandler = new JwtSecurityTokenHandler();
 
             var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            var claimList = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Email, applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, applicationUser.UserName)
+            };
         }
     }
 }
