1cb781e912f9d3fae88f8e4f18848d66e7e16e6a
mreshboboyev <mreshboboyev@gmail.com>
2024-01-06 19:44:22 +0500

    Creating Service/IService/IJwtTokenGenerator.cs and method : GenerateToken()

---
+++ Mango.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
