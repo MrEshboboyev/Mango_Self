c798869884d91290a96a59780121cb2a6ee7a713
mreshboboyev <mreshboboyev@gmail.com>
2024-01-06 19:02:27 +0500

    Register() method return type edited : UserDto -> string (Purpose : View details this acton)

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -26,7 +26,7 @@ namespace Mango.Services.AuthAPI.Service
             throw new NotImplementedException();
         }
 
-        public async Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        public async Task<string> Register(RegistrationRequestDto registrationRequestDto)
         {
             ApplicationUser user = new()
             {
@@ -53,14 +53,18 @@ namespace Mango.Services.AuthAPI.Service
                         PhoneNumber = userToReturn.PhoneNumber
                     };
 
-                    return userDto;
+                    return "";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().ToString();
                 }
             }
             catch (Exception ex)
             {
 
             }
-            return new UserDto();
+            return "Error encountered";
         }
     }
 }
