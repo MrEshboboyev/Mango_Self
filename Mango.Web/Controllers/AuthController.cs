a0b43f45754575301f021fca7071a103c1fff3f4
mreshboboyev <mreshboboyev@gmail.com>
2024-01-07 00:04:37 +0500

    [HttpPost] method for Register() action in AuthController.cs

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -34,6 +34,37 @@ namespace Mango.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if(result != null && result.IsSuccess)
+            {
+                if(string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if(assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration successfully!";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem() { Text = SD.RoleAdmin, Value =  SD.RoleAdmin},
+                new SelectListItem() { Text = SD.RoleCustomer, Value =  SD.RoleCustomer}
+            };
+
+            ViewBag.roleList = roleList;
+            return View(obj);
+        }
+
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
