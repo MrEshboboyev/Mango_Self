ebdd2402952208beb4526eced94bd297de7e1ed4
mreshboboyev <mreshboboyev@gmail.com>
2024-01-07 15:39:05 +0500

    Creating Extensions/WebApplicationBuilderExtensions.cs and copied from (ApiSettings in Program.cs) to AddAppAuthentication() method writed

---
+++ Mango.Services.CouponAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.Services.CouponAPI.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            // configuring ApiSettings
+            var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+            var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
+            var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            // add authentication with Jwt Bearer
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience
+                };
+            });
+
+            return builder;
+        }
+    }
+}
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -53,30 +54,7 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
-// configuring ApiSettings
-var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
-var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
-var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
-
-var key = Encoding.ASCII.GetBytes(secret);
-
-// add authentication with Jwt Bearer
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidIssuer = issuer, 
-        ValidAudience = audience
-    };
-});
+builder.AddAppAuthentication();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
