8d129ad14623138f4d2f634b12d4acfc6a4c7177
mreshboboyev <mreshboboyev@gmail.com>
2024-01-07 22:36:23 +0500

    Creating CartAPIController.cs type of ["Api empty"];  DI for AppDbContext, Mapper; ResponseDto created new in Constructor

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+        private ResponseDto _response;
+
+        public CartAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            this._response = new ResponseDto();
+        }
+
+
+
+    }
+}
