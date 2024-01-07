242a8d409ab0189bff6b6c9a9a93e6fdceedc822
mreshboboyev <mreshboboyev@gmail.com>
2024-01-07 15:15:33 +0500

    Services added : AddAuthentication with AddJwtBearer in Program.cs

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
 using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,6 +33,25 @@ var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
 var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
 
 var key = Encoding.ASCII.GetBytes(secret);
+
+// add authentication with Jwt Bearer
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = issuer, 
+        ValidAudience = audience
+    };
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
