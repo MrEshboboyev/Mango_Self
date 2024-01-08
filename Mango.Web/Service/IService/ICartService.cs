90d73e3a31a0381bf7dbccdc179853e0b69f058a
mreshboboyev <mreshboboyev@gmail.com>
2024-01-08 23:17:04 +0500

    Writing ICartService methods : GetCartByUserIdAsync(), UpsertCartAsync(), RemoveFromCartAsync(), ApplyCouponAsync()

---
+++ Mango.Web/Service/IService/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCardByUserIdAsync(string userId);
+        Task<ResponseDto?> UpsertCardByUserIdAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+    }
+}
