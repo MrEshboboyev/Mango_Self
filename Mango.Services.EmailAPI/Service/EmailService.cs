fb8e9a2ab86b776c905793a54a3e5635e583e838
mreshboboyev <mreshboboyev@gmail.com>
2024-01-10 15:28:21 +0500

    Create Service/EmailService.cs and inherit from IEmailService.cs : method -> EmailCartAndLog()

---
+++ Mango.Services.EmailAPI/Service/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.EmailAPI.Models.Dto;
+
+namespace Mango.Services.EmailAPI.Service
+{
+    public class EmailService : IEmailService
+    {
+        public Task EmailCartAndLog(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
